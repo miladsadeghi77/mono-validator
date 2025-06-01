c6c032de3603ad7f7175277675bcf7c2609b9a74
MiladSadeghi <miladpci88@gmail.com>
2025-06-01 09:41:19 +0330

    Refactor(NotEmptyValidator): change if to switch case

--- src/main/java/com/sadeghi/validation/validator/impl/NotEmptyValidator.java
+++ src/main/java/com/sadeghi/validation/validator/impl/NotEmptyValidator.java
@@ -16,17 +16,24 @@ public class NotEmptyValidator implements BaseValidator<NotEmpty, Object> {
   @Override
   public boolean isValid(Object value) {
 
-    if (value == null ) return false;
-
-    if(value instanceof String valueString) return !valueString.trim().isEmpty();
-
-    if (value instanceof Collection valueCollection) return !valueCollection.isEmpty();
-
-    if (value instanceof Map valueMap) return !valueMap.isEmpty();
+    switch (value) {
+      case null -> {
+        return false;
+      }
+      case String valueString -> {
+        return !valueString.trim().isEmpty();
+      }
+      case Collection valueCollection -> {
+        return !valueCollection.isEmpty();
+      }
+      case Map valueMap -> {
+        return !valueMap.isEmpty();
+      }
+      default -> {
+      }
+    }
 
     if (value.getClass().isArray()) return Array.getLength(value) == 0;
     return false;
-
-
   }
 }
